61ca703d5c15d69ee6d71039a3a7899300bf9cbe
rianDevRain <mail@rian.dev>
2020-10-06 20:42:28 -0500

    added reset and styling and functions
    Added reset button for extra credit.
    Tried to change functions and learning arrow functions.
    Added some styling.

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # diceRoller
  JavaScript Assignment
 
-2 sites that helped me write the code are as follows. I don't feel I copied their code except for the simple add function which I use to add up the array. The second site shows a simple way to use the .map() function to create a new array to list out as list items in html.
+Here are 2 sites that helped me write my code the most. I used the functions in both of them.
+
+The first one I found how to use the .reduce() with an add function
 
 https://medium.com/@chrisburgin95/rewriting-javascript-sum-an-array-dbf838996ed0
 
--- script.js
+++ script.js
@@ -1,34 +1,75 @@
-let howManyBtn = document.querySelector("#submitDice");
-let numberOfDie = document.querySelector("#numberOfDice");
-let totalOfDice = document.querySelector("#totalDie");
-let showBtn = document.querySelector("#showAllBtn");
-let showArrayList = document.querySelector("#showArray");
+/** @format */
 
-let dieRolls = [];
+// Get all the items I need from html
+const howManyBtn = document.querySelector("#submitDice");
+const numberOfDie = document.querySelector("#numberOfDice");
+const totalOfDice = document.querySelector("#totalDie");
+const showBtn = document.querySelector("#showAllBtn");
+const showArrayList = document.querySelector("#showArray");
+const resetButton = document.querySelector("#reset");
+const formContainer = document.querySelector("#form");
+// declare the dieRolls array. Const can be mutated later
+const dieRolls = [];
+//Event listeners call functions below
+resetButton.addEventListener("click", resetLoop);
+howManyBtn.addEventListener("click", diceAdder);
+showBtn.addEventListener("click", arrayDisplay);
+// diceAdder takes the value from input and checks if it is a number
+//Then it generates a random number between 1-6 and pushes to array
+//Then using reduce method and a add function adds each item of the
+//array and displays to html
+function diceAdder() {
+	let dieNumber = Number(numberOfDie.value);
+	if (isNaN(dieNumber)) {
+		totalOfDice.innerHTML = `Please enter a number`;
+	} else {
+		let count = 0;
+		while (count < dieNumber) {
+			dieRolls.push(Math.floor(Math.random() * 6) + 1);
+			count++;
+		}
+		// Loops through each item in an array and adds each item to the total.
+		//I changed to an arrow function all on one line.
+		//Explaned below above the add function
+		let sum = dieRolls.reduce((total, itemInArray) => total + itemInArray);
+		//The other way from the source showed this:
+		// const = dieRolls.reduce(add);
+		totalOfDice.innerHTML = `The total dice is: ${sum}`;
+	}
+}
+//This function uses the map() method to loop through each item and display
+//in html each die in an ordered list. Each loop it is writing the html
+//For each item in the array I understand there are
+function arrayDisplay() {
+	showArrayList.innerHTML =
+		"<ol>" +
+		dieRolls
+			.map(function (number) {
+				return "<li>" + number + "</li>";
+			})
+			.join("") +
+		"</ol>";
+}
+//Tied to the reset button this will pop each item from the array
+//Then resets all elements in html except for the input doesnt reset
+//See readme
+function resetLoop() {
+	while (dieRolls.length > 0) {
+		dieRolls.pop();
+	}
+	totalOfDice.innerHTML = `Enter a new Number`;
+	formContainer.reset();
+	arrayDisplay();
+}
+// Function for the reduce() method I got from source. Found out how to put this
+// directly into the reduce() method using =>. Basically it is taking each item b and
+// adding it to the total a. It does this for each item so it loops same amount
+// of times as there are items in array
 
-howManyBtn.addEventListener("click", () => {
-  let dieNumber = Number(numberOfDie.value);
-  if (isNaN(dieNumber)) {
-    totalOfDice.innerHTML = `Please enter a number`;
-  } else {
-    let count = 0;
-    while (count < dieNumber) {
-      dieRolls.push(Math.floor(Math.random() * 6));
-      count++;
-      // debugger;
-      const sum = dieRolls.reduce(add);
-      totalOfDice.innerHTML = `The total of all dice is: ${sum}`;
-    }
-  }
-});
+// const add = (a, b) => a + b;
 
-showBtn.addEventListener("click", () => {
-  showArrayList.innerHTML =
-    "<ol>" + dieRolls.map(function (number) {
-        return "<li>" + number + "</li>";
-      })
-      .join("") +
-    "</ol>";
-});
+// Same as if I did this:
 
-const add = (a, b) => a + b;
+// function add(total, item) {
+//   return total + item;
+// }
