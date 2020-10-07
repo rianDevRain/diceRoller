7fb2f338888e2e910228fba46f7d530b3a86e0c2
rianDevRain <mail@rian.dev>
2020-10-07 18:22:04 -0500

    Arrows Arrows Everywhere!!!

--- script.js
+++ script.js
@@ -11,14 +11,12 @@ const formContainer = document.querySelector("#form");
 // declare the dieRolls array. Const can be mutated later
 const dieRolls = [];
 //Event listeners call functions below
-resetButton.addEventListener("click", resetLoop);
-howManyBtn.addEventListener("click", diceAdder);
-showBtn.addEventListener("click", arrayDisplay);
+
 // diceAdder takes the value from input and checks if it is a number
 //Then it generates a random number between 1-6 and pushes to array
 //Then using reduce method and a add function adds each item of the
 //array and displays to html
-function diceAdder() {
+let diceAdder = () => {
 	let dieNumber = Number(numberOfDie.value);
 	if (isNaN(dieNumber)) {
 		totalOfDice.innerHTML = `Please enter a number`;
@@ -40,7 +38,7 @@ function diceAdder() {
 //This function uses the map() method to loop through each item and display
 //in html each die in an ordered list. Each loop it is writing the html
 //For each item in the array I understand there are
-function arrayDisplay() {
+let arrayDisplay = () => {
 	showArrayList.innerHTML =
 		"<ol>" +
 		dieRolls
@@ -53,7 +51,7 @@ function arrayDisplay() {
 //Tied to the reset button this will pop each item from the array
 //Then resets all elements in html except for the input doesnt reset
 //See readme
-function resetLoop() {
+let resetLoop = () => {
 	while (dieRolls.length > 0) {
 		dieRolls.pop();
 	}
@@ -61,10 +59,17 @@ function resetLoop() {
 	formContainer.reset();
 	arrayDisplay();
 }
+
+
+resetButton.addEventListener("click", resetLoop);
+howManyBtn.addEventListener("click", diceAdder);
+showBtn.addEventListener("click", arrayDisplay);
 // Function for the reduce() method I got from source. Found out how to put this
 // directly into the reduce() method using =>. Basically it is taking each item b and
 // adding it to the total a. It does this for each item so it loops same amount
 // of times as there are items in array
+
+// Directly from source:
 
 // const add = (a, b) => a + b;
 
